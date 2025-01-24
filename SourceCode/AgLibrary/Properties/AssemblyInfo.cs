1a4e3aeb902fe91f773c52ffe728bbb312cf2cb7
FantasyTeddy <matthiasbaer@bluewin.ch>
2025-01-24 06:56:36 +0100

    Remove GItVersion.MSBuild NuGet package

--- SourceCode/AgLibrary/Properties/AssemblyInfo.cs
+++ SourceCode/AgLibrary/Properties/AssemblyInfo.cs
@@ -22,4 +22,15 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("692b4970-37f1-4f02-8f7f-a514283fc399")]
 
-// Versioning is handled by the GitVersion.MsBuild NuGet package
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- SourceCode/GPS/Properties/AssemblyInfo.cs
+++ SourceCode/GPS/Properties/AssemblyInfo.cs
@@ -21,4 +21,15 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("43d3e75e-dc8b-4c3a-8931-9495e1467d01")]
 
-// Versioning is handled by the GitVersion.MsBuild NuGet package
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
