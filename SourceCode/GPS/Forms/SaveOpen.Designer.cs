db149e2ef6604c37f46144a7474b4b1a5e2877bc
p00qwerty Gamer <p00qwerty@outlook.com>
2025-02-07 08:59:05 +0100

    skip headland when no boundary

--- SourceCode/GPS/Forms/SaveOpen.Designer.cs
+++ SourceCode/GPS/Forms/SaveOpen.Designer.cs
@@ -1153,6 +1153,7 @@ namespace AgOpenGPS
                                     }
                                 }
                             }
+                            else break;
                         }
                     }
 
